a1c6f77699bbf3a2e7c83aac7ccf3d4abff661e7
Nico H <asd@gmx.de>
2016-01-25 14:50:16 +0100

    First Templates

--- silex/web/templates/layout.html.php
+++ silex/web/templates/layout.html.php
@@ -1,7 +1,59 @@
-<h1>
-    <?php $view['slots']->output('title', 'Default title') ?>
-</h1>
+<?php
+/**
+ * @var $slots \Symfony\Component\Templating\Helper\SlotsHelper
+ *
+ */
+$slots = $view['slots'];
+?>
 
-<hr/>
-<?php $view['slots']->output('_content') ?>
-<hr/>
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="/vendor/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/vendor/bootstrap/dist/js/bootstrap.min.js">
+        <link rel="stylesheet" href="/vendor/jquery/dist/jquery.min.js">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    </head>
+
+    <body>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Project name</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li <?= $active == 'home' ? 'class = "active"' : '';?>><a href="/home">Home</a></li>
+                    <li <?= $active == 'about' ? 'class = "active"' : '';?>><a href="/about">About</a></li>
+                    <li <?= $active == 'blog' ? 'class = "active"' : '';?>><a href="/blog">Blog</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li class="dropdown-header">Nav header</li>
+                            <li><a href="#">Separated link</a></li>
+                            <li><a href="#">One more separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+
+
+    <?php $slots->output('_content') ?>
+
+    </body>
+</html>
